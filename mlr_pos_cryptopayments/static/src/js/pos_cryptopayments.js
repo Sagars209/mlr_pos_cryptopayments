fe8dff3efde5444b951160a620d9f8e940fd3d56
sagar <sagars209@gmail.com>
2025-01-13 20:56:02 +0530

    fixed issues for odoo v18

--- mlr_pos_cryptopayments/static/src/js/pos_cryptopayments.js
+++ mlr_pos_cryptopayments/static/src/js/pos_cryptopayments.js
@@ -1,51 +1,40 @@
 /** @odoo-module */
 import { patch } from "@web/core/utils/patch";
-import { Payment } from "@point_of_sale/app/store/models";
+//import { PosStore } from "@point_of_sale/app/store/pos_store";
+import { PosPayment } from "@point_of_sale/app/models/pos_payment";
 
-patch(Payment.prototype, {
+patch(PosPayment.prototype, {
     //exports as JSON for receipt printing
-    export_for_printing() {
-        let data = super.export_for_printing();
+    export_for_printing(baseUrl, headerData) {
+        console.log('checking if it call this export_for_printing');
+        console.error('lets print all values');
+        console.log(this.cryptopay_payment_link_qr_code);
+        console.log(this.conversion_rate);
+        let data = super.export_for_printing(baseUrl, headerData);
         data.cryptopay_payment_link_qr_code = this.cryptopay_payment_link_qr_code;
         data.conversion_rate = this.conversion_rate;
         data.invoiced_crypto_amount = this.invoiced_crypto_amount;
         return data;    
     },
-    init_from_JSON(json) {
-        console.log('Calling Init_from_Json for payment');
-        console.log(json);
-        super.init_from_JSON(...arguments);
+    setup(vals) {
+        super.setup(...arguments);
+        console.log('called from new setup()');
+        console.log(vals);
         try {
-            this.is_crypto_payment = json.is_crypto_payment;
-            console.log('is_crypto_payment:'+json.is_crypto_payment)
-            this.cryptopay_invoice_id = json.cryptopay_invoice_id;
-            this.cryptopay_payment_link = json.cryptopay_payment_link;
-            this.cryptopay_payment_type = json.cryptopay_payment_type;
-            this.conversion_rate = json.conversion_rate;
+            this.is_crypto_payment = vals.is_crypto_payment;
+            console.log('is_crypto_payment:'+vals.is_crypto_payment)
+            this.cryptopay_invoice_id = vals.cryptopay_invoice_id;
+            this.cryptopay_payment_link = vals.cryptopay_payment_link;
+            this.cryptopay_payment_type = vals.cryptopay_payment_type;
+            this.conversion_rate = vals.conversion_rate;
             const codeWriter = new window.ZXing.BrowserQRCodeSvgWriter();
-            let qr_code_svg = new XMLSerializer().serializeToString(codeWriter.write(json.cryptopay_payment_link, 150, 150));
+            let qr_code_svg = new XMLSerializer().serializeToString(codeWriter.write(vals.cryptopay_payment_link, 150, 150));
             this.cryptopay_payment_link_qr_code = "data:image/svg+xml;base64,"+ window.btoa(qr_code_svg);
-            this.invoiced_crypto_amount = json.invoiced_crypto_amount;
+            this.invoiced_crypto_amount = vals.invoiced_crypto_amount;
         }
         catch (error) {
             console.log(error);
             return false;
         }
-    },
-    export_as_JSON() {
-        let json = super.export_as_JSON(...arguments);
-        console.log('super export as json');
-        console.log(json)
-        json.is_crypto_payment = this.is_crypto_payment;
-        console.log('crypto payment type '+json.is_crypto_payment)
-        json.cryptopay_payment_type = this.cryptopay_payment_type;
-        json.cryptopay_invoice_id = this.cryptopay_invoice_id;
-        json.cryptopay_payment_link_qr_code = this.cryptopay_payment_link_qr_code;
-        json.cryptopay_payment_link = this.cryptopay_payment_link;
-        json.invoiced_crypto_amount = this.invoiced_crypto_amount;
-        json.conversion_rate = this.conversion_rate;
-        console.log('calling export_as_JSON');
-        console.log(json);
-        return json;
     },
 });
